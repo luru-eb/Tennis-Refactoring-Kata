e07c4802603c94dec8215c43830d321b54420907
luru <luru@eventbrite.com>
2022-10-28 12:15:37 +0200

    Refactor to classes

--- python/tennis.py
+++ python/tennis.py
@@ -1,69 +1,90 @@
     # -*- coding: utf-8 -*-
+from abc import ABC, abstractmethod
 
 class TennisGame1:
 
     def __init__(self, player1Name, player2Name):
-        self.player1Name = player1Name
-        self.player2Name = player2Name
-        self.player1Points = 0
-        self.player2Points = 0
+        self.player1 = Player(player1Name)
+        self.player2 = Player(player2Name)
         
     def won_point(self, playerName):
-        if playerName == self.player1Name:
-            self.player1Points += 1
+        if playerName == self.player1.name:
+            self.player1.add_point()
         else:
-            self.player2Points += 1
+            self.player2.add_point()
     
     def _is_draw_result(self):
-        return self.player1Points == self.player2Points
+        return self.player1.points == self.player2.points
+
+    def _is_regular_result(self):
+        return self.player1.points < 4 and self.player2.points < 4
+
+    def _is_advantage_result(self):
+        return abs(self.player1.points - self.player2.points) == 1
+
+    def score(self):
+        if self._is_draw_result():
+            return DrawScore().score(self.player1, self.player2)
         
-    def _get_draw_score(self):
+        if self._is_regular_result():
+            return RegularScore().score(self.player1, self.player2)
+
+        if self._is_advantage_result():
+            return AdvantageScore().score(self.player1, self.player2)
+            
+        return WinScore().score(self.player1, self.player2)
+
+
+class Referee:
+    pass
+
+
+class Score(ABC):
+    
+    @abstractmethod
+    def score(self, player1, player2):
+        pass
+
+
+class DrawScore(Score):
+    def score(self, player1, player2):
         return {
             0 : "Love-All",
             1 : "Fifteen-All",
             2 : "Thirty-All",
-        }.get(self.player1Points, "Deuce")
-    
-    def _get_regular_score(self):
+        }.get(player1.points, "Deuce")
+
+
+class AdvantageScore(Score):
+    def score(self, player1, player2):
+        if (player1.points - player2.points == 1):
+            return "Advantage " + player1.name
+        return "Advantage " + player2.name
+
+
+class RegularScore(Score):
+    def score(self, player1, player2):
         regular_score = {
             0 : "Love",
             1 : "Fifteen",
             2 : "Thirty",
             3 : "Forty",
         }
-        return f"{regular_score[self.player1Points]}-{regular_score[self.player2Points]}"
+        return f"{regular_score[player1.points]}-{regular_score[player2.points]}"
 
-    def _is_regular_result(self):
-        return self.player1Points < 4 and self.player2Points < 4
 
-    def _is_advantage_result(self):
-        return abs(self.player1Points - self.player2Points) == 1
-    
-    def _is_win_candidate(self):
-        return abs(self.player1Points - self.player2Points) == 1
-    
-    def _get_advantage_score(self):
-        minusResult = self.player1Points - self.player2Points
-        if (minusResult == 1):
-            return "Advantage " + self.player1Name
-        return "Advantage " + self.player2Name
+class WinScore(Score):
+    def score(self, player1, player2):
+        if (player1.points - player2.points >= 2):
+            return "Win for " + player1.name
+        return "Win for " + player2.name
 
-    def _get_win_score(self):
-        minusResult = self.player1Points - self.player2Points
 
-        if (minusResult >= 2):
-            return "Win for " + self.player1Name
-        return "Win for " + self.player2Name
+class Player:
 
-    def score(self):
-        if self._is_draw_result():
-            return self._get_draw_score()
-        
-        if self._is_regular_result():
-            return self._get_regular_score()
+    def __init__(self, name):
+        self.name = name
+        self.points = 0
 
-        if self._is_advantage_result():
-            return self._get_advantage_score()
-            
-        return self._get_win_score()
-        
+    def add_point(self):
+        self.points+=1
